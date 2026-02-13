61aa2b3bf19bc8df091da3f940c94bb3e6d21fbb
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-13 10:33:51 +0530

    new-sserver last week

--- index.js
+++ index.js
@@ -1,4 +1,147 @@
+// const express = require("express");
+// const app = express();
+// app.use(express.json());
+// const cg = [
+//   {
+//     studentName: "ABDUL HAQUE",
+//     University: "SUxCG 714",
+//     UniversityUID: "108444",
+//   },
+//   {
+//     studentName: "ADITYA KUMAR",
+//     University: "SUxCG 702",
+//     UniversityUID: "108716",
+//   },
+//   {
+//     studentName: "AMAN KUMAR",
+//     University: "SUxCG 702",
+//     UniversityUID: "108500",
+//   },
+//   {
+//     studentName: "AMRIT RAJ",
+//     University: "SUxCG 702",
+//     UniversityUID: "108587",
+//   },
+//   {
+//     studentName: "ANISHA CHHAJER",
+//     University: "SUxCG 714",
+//     UniversityUID: "108565",
+//   },
+//   {
+//     studentName: "PRASHANT PARMAR",
+//     University: "SUxCG 714",
+//     UniversityUID: "108697",
+//   },
+//   {
+//     studentName: "PRITESH BACHHAV",
+//     University: "SUxCG 714",
+//     UniversityUID: "108654",
+//   },
+//   {
+//     studentName: "RISHI BHAII",
+//     University: "SUxCG 714",
+//     UniversityUID: "108657",
+//   },
+//   {
+//     studentName: "ANSHUUUU",
+//     University: "SUxCG 714",
+//     UniversityUID: "108254",
+//   },
+// ];
+
+// app.get("/", (req, res) => {
+//   res.send("Express server is running");
+// });
+
+// app.get("/cg", (req, res) => {
+//   res.status(200).json(cg);
+// });
+
+// app.get("/cg/students", (req, res) => {
+//   const names = cg.map(student => student.studentName);
+//   res.status(200).json(names);
+// });
+
+// app.post("/cg", (req, res) => {
+//   const user1 = {
+//     id: cg.length + 1,
+//     studentName: req.body[0].studentName,
+//     University: req.body[0].University,
+//     UniversityUID: req.body[0].UniversityUID
+//   };
+//   const user2 = {
+//     id: cg.length + 2,
+//     studentName: req.body[1].studentName,
+//     University: req.body[1].University,
+//     UniversityUID: req.body[1].UniversityUID
+//   };
+//   const user3 = {
+//     id: cg.length + 3,
+//     studentName: req.body[2].studentName,
+//     University: req.body[2].University,
+//     UniversityUID: req.body[2].UniversityUID
+//   };
+// console.log("user",user1);
+// console.log("user",user2);
+// console.log("user",user3);
+
+//   cg.push(user1);
+//   cg.push(user2);
+//   cg.push(user3);
+  
+// res.status(201).send("User created successfully");
+
+//   // res.status(201).json({
+//   //   message: "Student created",
+//   //   user: newUser
+//   // });
+
+// });
+
+
+
+// app.get("/cg/students/:gr_number", (req, res) => {
+//   const { gr_number } = req.params;
+//   const student = cg.find(s => s.UniversityUID === gr_number);
+//   if (!student) {
+//     return res.status(404).json({
+//       message: "Student not found",
+//     });
+//   }
+//   res.status(200).json(student);
+// });
+
+// app.get("/cg/students/name/:studentName", (req, res) => {
+//   const { studentName } = req.params;
+//   const student = cg.find(s => s.studentName.toLowerCase() === studentName.toLowerCase());
+
+//   if (!student) {
+//     return res.status(404).json({
+//       message: "Student not found",
+//     });
+//   }
+//   res.status(200).json(student);
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server started on port 3000");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
+const app = express();
+app.use(express.json());
 
 const cg = [
   {
@@ -36,9 +179,17 @@ const cg = [
     University: "SUxCG 714",
     UniversityUID: "108654",
   },
+  {
+    studentName: "RISHI BHAII",
+    University: "SUxCG 714",
+    UniversityUID: "108657",
+  },
+  {
+    studentName: "ANSHUUUU",
+    University: "SUxCG 714",
+    UniversityUID: "108254",
+  },
 ];
-
-const app = express();
 
 app.get("/", (req, res) => {
   res.send("Express server is running");
@@ -49,30 +200,58 @@ app.get("/cg", (req, res) => {
 });
 
 app.get("/cg/students", (req, res) => {
-  const names = cg.map(student => student.studentName);
+  const names = cg.map((student) => student.studentName);
   res.status(200).json(names);
+});
+
+app.post("/cg", (req, res) => {
+  const newUsers = [];
+
+  req.body.forEach((student, index) => {
+    const newUser = {
+      id: cg.length + index + 1,
+      studentName: student.studentName,
+      University: student.University,
+      UniversityUID: student.UniversityUID,
+    };
+
+    cg.push(newUser);
+    newUsers.push(newUser);
+  });
+
+  res.status(201).json({
+    message: "Students added successfully",
+    users: newUsers,
+  });
 });
 
 app.get("/cg/students/:gr_number", (req, res) => {
   const { gr_number } = req.params;
-  const student = cg.find(s => s.UniversityUID === gr_number);
+
+  const student = cg.find(
+    (s) => s.UniversityUID === gr_number
+  );
+
   if (!student) {
     return res.status(404).json({
       message: "Student not found",
     });
   }
+
   res.status(200).json(student);
 });
 
 app.get("/cg/students/name/:studentName", (req, res) => {
   const { studentName } = req.params;
-  const student = cg.find(s => s.studentName.toLowerCase() === studentName.toLowerCase());
+
+  const student = cg.find((s) => s.studentName.toLowerCase() === studentName.toLowerCase());
 
   if (!student) {
     return res.status(404).json({
       message: "Student not found",
     });
   }
+
   res.status(200).json(student);
 });
 
